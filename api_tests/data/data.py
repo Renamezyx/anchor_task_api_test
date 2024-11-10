7da36a00ef9ff5c159edb25795998cc543304314
Therzyx <2274466264@qq.com>
2024-11-10 20:59:56 +0800

    create datas generate

--- api_tests/data/data.py
+++ api_tests/data/data.py
@@ -3,11 +3,14 @@ from api_tests.utils.xls_tools import XlsTools
 
 class Data:
     def __init__(self, path_task_to_anchor_datas="../data/data.xlsx"):
-        self.task_to_anchor_datas = Data.get_task_to_anchor_datas(path_task_to_anchor_datas)
-    @staticmethod
-    def get_task_to_anchor_datas(path_task_to_anchor_datas):
+        self.path_task_to_anchor_datas = path_task_to_anchor_datas
+        self.task_to_anchor_datas = None
+        self.title = None
+        self.get_task_to_anchor_datas()
+
+    def get_task_to_anchor_datas(self):
         task_to_anchor_datas = []
-        xls = XlsTools(path_task_to_anchor_datas)
+        xls = XlsTools(self.path_task_to_anchor_datas)
         all_rows = xls.read_all_rows()  # 只读取一次所有行
         keys = all_rows[0]  # 第一行作为键
         values = all_rows[1:]  # 后续行作为值
@@ -18,14 +21,27 @@ class Data:
                 key = keys[idx]
                 item[key] = v
             task_to_anchor_datas.append(item)
-
-
-        return task_to_anchor_datas
+        self.task_to_anchor_datas = task_to_anchor_datas
+        self.title = keys
 
 
 if __name__ == "__main__":
     o_datas = Data()
     datas = o_datas.task_to_anchor_datas
     anchor_task_for_levels = {}
-    for data in datas:
-
+    task_levels = [i for i in o_datas.title if "anchor_task_level_" in i]
+    for i in datas:
+        print(i)
+    tasks_detail = []
+    for task_level in task_levels:
+        for data in datas:
+            for i in data:
+                if i in task_levels:
+                    tasks_detail.append({'anchor_level': data["anchor_level"],
+                                         'task_name': data["task_name"],
+                                         'task_key': data["task_key"],
+                                         'anchor_task_level': i.replace("anchor_task_level_", ""),
+                                         'value': data[i]})
+    for i in tasks_detail:
+        print(i)
+    print(len(tasks_detail),len(datas))
---
+++ api_tests/data/data_anchor_level_require.py
@@ -0,0 +1,238 @@
+import copy
+import random
+
+anchor_level_require_max = {
+    "1": [
+        {
+            "level": 1,
+            'regin': '',
+            'Last7DayAvgAcu': [0, 0],
+            'Last1DayFansCnt': [0, 3000 - 1],
+            'Last7DayWatchTotalDuration': [0, 20 * 60 - 1],
+            'Last30DayIncome': [0, 0],
+            'Last7DayIncome': [0, 0],
+            'condition': 'and'
+        }
+    ],
+    "2": [
+        {
+            "level": 2,
+            'regin': '',
+            'Last7DayAvgAcu': [0, 1],
+            'Last1DayFansCnt': [3 * 1000, 10 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [20 * 60, 999999999],
+            'Last30DayIncome': [1, 3000 - 1],
+            'Last7DayIncome': [1, 1000 - 1],
+            'condition': 'or'
+        }
+    ],
+    "3": [
+        {
+            "level": 3,
+            'regin': 'US',
+            'Last7DayAvgAcu': [2, 2],
+            'Last1DayFansCnt': [5 * 1000, 10 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [3 * 1000, 20 * 1000 - 1],
+            'Last7DayIncome': [1 * 1000, 5 * 1000 - 1],
+            'condition': 'or'
+        },
+        {
+            "level": 3,
+            'regin': 'JP',
+            'Last7DayAvgAcu': [2, 2],
+            'Last1DayFansCnt': [5 * 1000, 10 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [3 * 1000, 20 * 1000 - 1],
+            'Last7DayIncome': [1 * 1000, 5 * 1000 - 1],
+            'condition': 'or'
+        },
+        {
+            "level": 3,
+            'regin': '',
+            'Last7DayAvgAcu': [2, 2],
+            'Last1DayFansCnt': [5 * 1000, 10 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [3 * 1000, 7.5 * 1000 - 1],
+            'Last7DayIncome': [1 * 1000, 2 * 1000 - 1],
+            'condition': 'or'
+        }
+    ],
+    "4": [
+        {
+            "level": 4,
+            'regin': 'US',
+            'Last7DayAvgAcu': [3, 4],
+            'Last1DayFansCnt': [10 * 1000, 15 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [20 * 1000, 40 * 1000 - 1],
+            'Last7DayIncome': [5 * 1000, 10 * 1000 - 1],
+            'condition': 'or'
+        },
+        {
+            "level": 4,
+            'regin': 'JP',
+            'Last7DayAvgAcu': [3, 4],
+            'Last1DayFansCnt': [10 * 1000, 15 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [20 * 1000, 40 * 1000 - 1],
+            'Last7DayIncome': [5 * 1000, 10 * 1000 - 1],
+            'condition': 'or'
+        },
+        {
+            "level": 4,
+            'regin': '',
+            'Last7DayAvgAcu': [3, 4],
+            'Last1DayFansCnt': [10 * 1000, 15 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [7.5 * 1000, 15 * 1000 - 1],
+            'Last7DayIncome': [2 * 1000, 4 * 1000 - 1],
+            'condition': 'or'
+        }
+    ],
+    "5": [
+        {
+            "level": 5,
+            'regin': 'US',
+            'Last7DayAvgAcu': [3, 29],
+            'Last1DayFansCnt': [15 * 1000, 35 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [],
+            'Last30DayIncome': [40 * 1000, 600 * 1000 - 1],
+            'Last7DayIncome': [10 * 1000, 150 * 1000 - 1],
+            'condition': 'or'
+        },
+        {
+            "level": 5,
+            'regin': 'JP',
+            'Last7DayAvgAcu': [3, 29],
+            'Last1DayFansCnt': [15 * 1000, 35 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [20 * 60, 999999999],
+            'Last30DayIncome': [40 * 1000, 200 * 1000 - 1],
+            'Last7DayIncome': [10 * 1000, 50 * 1000 - 1],
+            'condition': 'or'
+        },
+        {
+            "level": 5,
+            'regin': '',
+            'Last7DayAvgAcu': [3, 29],
+            'Last1DayFansCnt': [15 * 1000, 35 * 1000 - 1],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [15 * 1000, 150 * 1000 - 1],
+            'Last7DayIncome': [4 * 1000, 40 * 1000 - 1],
+            'condition': 'or'
+        }
+    ],
+    "6": [
+        {
+            "level": 6,
+            'regin': 'US',
+            'Last7DayAvgAcu': [30, 999999999],
+            'Last1DayFansCnt': [35 * 1000, 999999999],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [600 * 1000, 999999999],
+            'Last7DayIncome': [150 * 1000, 999999999],
+            'condition': 'or'
+        },
+        {
+            "level": 6,
+            'regin': 'JP',
+            'Last7DayAvgAcu': [30, 999999999],
+            'Last1DayFansCnt': [35 * 1000, 999999999],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [200 * 1000, 999999999],
+            'Last7DayIncome': [50 * 1000, 999999999],
+            'condition': 'or'
+        },
+        {
+            "level": 6,
+            'regin': '',
+            'Last7DayAvgAcu': [30, 999999999],
+            'Last1DayFansCnt': [35 * 1000, 999999999],
+            'Last7DayWatchTotalDuration': [0],
+            'Last30DayIncome': [150 * 1000, 999999999],
+            'Last7DayIncome': [40 * 1000, 999999999],
+            'condition': 'or'
+        }
+    ]
+}
+indicator = ["Last7DayAvgAcu", "Last1DayFansCnt", "Last7DayWatchTotalDuration", "Last30DayIncome",
+             "Last7DayIncome"]
+
+
+def get_level_for_data(data):
+    level = 0
+    regin = data['regin']
+    for anchor_level_require in anchor_level_require_max.values():
+        flag = []
+        require = [i for i in anchor_level_require if i['regin'] == regin][0]
+        if len(require) == 0:
+            regin = ""
+            require = [i for i in anchor_level_require if i['regin'] == regin][0]
+        condition = all if require["condition"] == "and" else any
+        # print(f"---------{require["level"]}---------")
+        for k, v in require.items():
+            if k in indicator:
+                if len(v) <= 1:
+                    pass
+                else:
+                    flag.append(all([data[k] >= v[0], data[k] <= v[1]]))
+                    # print(f"{data[k]} >= {v[0]}, {data[k]} <= {v[1]}, {flag}")
+        if condition == all:
+            if condition(flag):
+                level = require["level"]
+                return level
+        else:
+            if condition(flag):
+                level = require["level"]
+                # print(f"{condition(flag), flag}")
+    return level
+
+
+def generate_boundary_values(level: str, regin: str = ""):
+    boundary_value = []
+
+    def set_boundary_value(item):
+        value_template = {}
+        values = []
+        for k, v in item.items():
+            if k in indicator:
+                value_template[k] = v[0]
+            else:
+                value_template[k] = v
+
+        def do_set(key, value):
+            t = copy.copy(value_template)
+            t[key] = value
+            values.append(t)
+
+        for k, v in item.items():
+            if k in indicator:
+                vl = v[0]
+                do_set(k, vl)
+                vl = v[1] if len(v) > 1 else v[0]
+                do_set(k, vl)
+                vl = v[0] - 1 if v[0] > 0 else 0
+                do_set(k, vl)
+                vl = v[1] + 1 if len(v) > 1 else v[0]
+                do_set(k, vl)
+
+                if len(v) == 1 and v == 0:
+                    vl = random.randint(0, 1000000)
+                    do_set(k, vl)
+
+        return [dict(t) for t in {tuple(d.items()) for d in values}]
+
+    for item in [i for i in anchor_level_require_max[level] if i["regin"]]:
+        if regin.lower() == item['regin'].lower():
+            boundary_value += set_boundary_value(item)
+    if not boundary_value:
+        boundary_value += set_boundary_value([i for i in anchor_level_require_max[level] if i["regin"] == ""][0])
+
+    return boundary_value
+
+
+if __name__ == '__main__':
+    t = generate_boundary_values("6")
+    for i in t:
+        i["level"] = get_level_for_data(i)
+        print(i)
